13d625e5f8feaaf2b57a8696aeae551948f05529
Feighen Oosterbroek <feighen@gmail.com>
2016-07-19 23:08:01 +0200

    finished setup. Starting on password function

--- src/public/index.php
+++ src/public/index.php
@@ -1,31 +1,55 @@
 <?php
 namespace Password;
+// : Defines
+defined('BASE') || define('BASE', substr(dirname(realpath(__FILE__)), 0, strrpos(dirname(realpath(__FILE__)), 'password')));
+// : End
 //: Includes
-require __DIR__ . '/vendor/autoload.php';
+require BASE . 'password' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 //: End
-//: Object
-class password
+class IndexController
 {
 	//: Variables
 	
 	//: End
 	//: Public functions
-	//: Magic functions
 	public function __construct()
 	{
-		//: Does the config exist?
-		print('sex');
-		if (file_exists(__DIR__ . 'config') === false) {
-			header('Location: setup.php');
-			exit;
-		}
-		//: End
+		//: Header
 		print(file_get_contents(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'header.html'));
-		
+		//: Content
+		if (file_exists(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'config') === false) {
+			$this->setMeUp();
+		} else {
+			$this->showPasswords();
+		}
+	}
+	
+	public function __destruct()
+	{
+		//: Footer
 		print(file_get_contents(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'footer.html'));
 	}
 	//: End
+	//: Private functions
+	private function setMeUp()
+	{
+		if (!empty($_POST)) {
+			$file = (string) dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'config';
+			$fp = fopen($file, 'w');
+			fwrite($fp, $_POST['remember_for']);
+			fclose($fp);
+			header('Location: /');
+		} else {
+			print(file_get_contents(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'setup.html'));
+		}
+	}
+	
+	private function showPasswords()
+	{
+		print('<div class="eleven columns">');
+		print('<img id="aj_load" alt="Ajax Loader" src="ajax-loader.gif" />');
+		print('</div>');
+	}
 	//: End
 }
-//: End
-$password = new \Password\password();
+$IndexController = new IndexController();
