994b1468d3803cbea223973c569535c6a81f03e7
Feighen Oosterbroek <feighen@gmail.com>
2016-07-19 21:35:44 +0200

    initial templates setup

--- src/public/index.php
+++ src/public/index.php
@@ -14,11 +14,15 @@ class password
 	public function __construct()
 	{
 		//: Does the config exist?
+		print('sex');
 		if (file_exists(__DIR__ . 'config') === false) {
 			header('Location: setup.php');
 			exit;
 		}
 		//: End
+		print(file_get_contents(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'header.html'));
+		
+		print(file_get_contents(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'footer.html'));
 	}
 	//: End
 	//: End
---
+++ src/public/setup.php
@@ -0,0 +1,6 @@
+<?php
+namespace Password;
+
+print(file_get_contents(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'header.html'));
+
+print(file_get_contents(dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'footer.html'));
